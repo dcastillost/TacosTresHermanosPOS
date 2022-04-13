cc985dc30473a0fd72790a3ddbe84acdf4795037
David Castillo <dcastillo.st@gmail.com>
2022-04-13 19:46:36 +0900

    Add axios to handle API requests, Refactoring

--- tacos-tres-hermanos-pos-ui/src/components/SearchBar.js
+++ tacos-tres-hermanos-pos-ui/src/components/SearchBar.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = {term: ''};
 
-  onInputChange(event) {
-    
-  }
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.term);
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
     <div className='ui segment'>
-      <form className='ui form'>
+      <form onSubmit={this.onFormSubmit} className='ui form'>
         <div className='field'>
           <label>Item search</label>
-          <input type='text' onChange={this.onInputChange}/>
+          <input 
+            type='text' 
+            value={this.state.term}
+            onChange={(e) => this.setState({term: e.target.value})} />
         </div>
       </form>
     </div>
