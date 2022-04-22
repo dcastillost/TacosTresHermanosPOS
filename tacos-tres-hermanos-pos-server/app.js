7944a7d642dba3f2b31733a06df0afcc84b6053a
David Castillo <dcastillo.st@gmail.com>
2022-04-22 12:20:25 +0900

    Complete basic API functionality for Menu endpoint

--- tacos-tres-hermanos-pos-server/app.js
+++ tacos-tres-hermanos-pos-server/app.js
@@ -1,3 +1,14 @@
+/*
+API SERVER
+
+Description:
+RESTful API server that handles HTTP requests.
+
+For refactoring:
+Always returns JSON.
+Error and exception handling.
+*/
+
 require("dotenv").config();
 const express = require('express');
 const bodyParser = require("body-parser");
@@ -38,6 +49,7 @@ database.once('connected', () => {
 //Mongo Schema for menu items
 const menuItemSchema = new mongoose.Schema({
   name: String,
+  slug: String,
   price: Number,
   shortDescription: String,
   longDescription: String,
@@ -51,22 +63,64 @@ const menuItemSchema = new mongoose.Schema({
 //Compile schema into a model
 const MenuItem = mongoose.model('MenuItem', menuItemSchema);
 
-//Get ALL menu items
-app.get('/menu', (req, res) => {
-  try {
-    MenuItem.find((err, menuItems) => {
+/*
+MENU ROUTING:
+-GET all menu items
+-POST one menu item
+-DELETE all menu items
+  //Add warning/extra security for this action.
+*/
+app.route('/menu')
+  .get((req, res) => {
+    try {
+      MenuItem.find((err, menuItems) => {
+        if (!err) {
+          res.json(menuItems);
+        }
+        else {
+          res.json(err);
+        }
+      });
+    }
+    catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  })
+  .post((req, res) => {
+    try {
+      const newMenuItem = new MenuItem({
+        name: req.body.name,
+        slug: req.body.slug,
+        price: req.body.price,
+        shortDescription: req.body.shortDescription,
+        longDescription: req.body.longDescription,
+        units: req.body.units,
+        options: parseInt(req.body.options),
+        category: req.body.category,
+        imageURL: req.body.imageURL,
+        availability: req.body.availability
+      });
+      newMenuItem.save(err => {
+        if (!err) {
+          res.send('New menu item added to database.')
+        } else {
+          res.send(err);
+        }
+      });
+    }
+    catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  })
+  .delete((req, res) => {
+    MenuItem.deleteMany((err) => {
       if (!err) {
-        res.json(menuItems);
-      }
-      else {
-        res.json(err);
+        res.send('All menu items have been deleted.');
+      } else {
+        res.send(err);
       }
     });
-  }
-  catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+  });
 
 //GET items based on category
 app.get('/menu/category', (req, res) => {
@@ -85,19 +139,28 @@ app.get('/menu/category', (req, res) => {
   }
 });
 
-//GET one item based on id
-
-//GET ALL orders
-
-//GET order based on id
-
-//POST order
-
-//POST new menu item
-app.post('/menu', (req, res) => {
-  try {
-    const newMenuItem = new MenuItem({
+/*
+MENU ITEM ROUTING
+-GET one menu item
+-PUT one menu item (update item from scratch)
+-DELETE one menu item
+-PATCH menu item / ingredient availability
+*/
+app.route('/menu/:menuItemSlug')
+  .get((req, res) => {
+    MenuItem.findOne({ slug: req.params.menuItemSlug }, (err, foundMenuItem) => {
+      if (foundMenuItem) {
+        res.json(foundMenuItem);
+      } else {
+        res.send('No item menu was found under that slug.')
+      }
+    });
+  })
+  .put((req, res) => {
+    const filter = { slug: req.params.menuItemSlug };
+    const replacement = {
       name: req.body.name,
+      slug: req.body.slug,
       price: req.body.price,
       shortDescription: req.body.shortDescription,
       longDescription: req.body.longDescription,
@@ -106,35 +169,60 @@ app.post('/menu', (req, res) => {
       category: req.body.category,
       imageURL: req.body.imageURL,
       availability: req.body.availability
-    });
-    newMenuItem.save(err => {
-      if(!err) {
-        res.send('New menu item added to database.')
+    };
+    MenuItem.replaceOne(filter, replacement, (err) => {
+      if (!err) {
+        res.send('Successfully replaced menu item.');
       } else {
+        // res.send('No matching items found.');
         res.send(err);
       }
     });
-  }
-  catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+  })
+  .delete((req, res) => {
+    filter = { slug: req.params.menuItemSlug };
+    MenuItem.deleteOne(filter, (err) => {
+      if (!err) {
+        res.send('Successfully deleted menu item.');
+      } else {
+        // res.send('No matching items found.');
+        res.send(err);
+      }
+    });
+  })
+  .patch((req, res) => {
+    const filter = { slug: req.params.menuItemSlug };
+    const updates = {
+      $set: {
+        name: req.body.name,
+        // slug: req.body.slug,
+        price: req.body.price,
+        // shortDescription: req.body.shortDescription,
+        // longDescription: req.body.longDescription,
+        // units: req.body.units,
+        // options: parseInt(req.body.options),
+        // category: req.body.category,
+        // imageURL: req.body.imageURL,
+        // availability: req.body.availability
+      }
+    };
+    MenuItem.updateOne(filter, updates, (err) => {
+      if (!err) {
+        res.send('Successfully updated menu item.');
+      } else {
+        // res.send('No matching items found.');
+        res.send(err);
+      }
+    });
+  });
+
+//GET ALL orders
+
+//GET order based on id
+
+//POST order
 
 //POST new ingredient
 
-//PATCH menu item / ingredient availability
-
 //Patch order status
-
-//DELETE all menu items
-//Add warning/extra security for this action.
-app.delete('/menu', (req, res) => {
-  MenuItem.deleteMany((err) => {
-    if(!err) {
-      res.send('All menu items have been deleted.');
-    } else {
-      res.send(err);
-    }
-  });
-});
 
