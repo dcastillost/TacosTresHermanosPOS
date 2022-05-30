c555cafd032bc2c24764660270834464cb1baacd
David Castillo <dcastillo.st@gmail.com>
2022-05-30 22:45:31 +0900

    Add 2 minute cache for all requests

--- tacos-tres-hermanos-pos-server/src/index.js
+++ tacos-tres-hermanos-pos-server/src/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const apicache = require('apicache');
 
 const v1MenuItemRouter = require('./v1/routes/menuItemRoutes');
 
 const app = express();
+const cache = apicache.middleware;
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cache("2 minutes"));
 app.use('/api/v1/menuitems', v1MenuItemRouter);
 
 app.listen(PORT, () => {
